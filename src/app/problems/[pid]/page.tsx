0691b572d0a34664341e4722e3225788ad7412d5
pelegsch666 <peleg.mazuz@gmail.com>
2023-06-21 16:25:45 +0300

    problem solved

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -4,9 +4,16 @@ import Workspace from '@/app/components/Workspace/Workspace';
 import { problems } from '@/app/utils/problems';
 import React from 'react';
 
-type ProblemPageProps = {};
 
-const ProblemPage = (props: ProblemPageProps) => {
+
+
+async function ProblemPage ({
+  params: { pid },
+}: {
+  params: { pid: string }
+}) {
+ const problem = problems[pid];
+  console.log(problem)
   return (
     <div>
       <Topbar problemPage />
@@ -20,31 +27,31 @@ export default ProblemPage;
 // fetch the local data
 // ssg - static site generation
 // getStaticPaths => it create the dynamic routes
-export async function getStaticPaths() {
-  const paths = Object.keys(problems).map(key => ({
-    params: { pid: key },
-  }));
+// export async function getStaticPaths() {
+//   const paths = Object.keys(problems).map(key => ({
+//     params: { pid: key },
+//   }));
 
-  return {
-    paths,
+//   return {
+//     paths,
    
-  };
-}
+//   };
+// }
 
-export async function getStaticProps({params} : {params:{pid: string}}){
-  const {pid} = params;
-  const problem = problems[pid];
-  if(!problem){
-    return{
-      notFound: true
-    }
-  }  
+// export async function getStaticProps({params} : {params:{pid: string}}){
+//   const {pid} = params;
+//   const problem = problems[pid];
+//   if(!problem){
+//     return{
+//       notFound: true
+//     }
+//   }  
 
- return {
-  props: {
-    problem
-  }
- }
+//  return {
+//   props: {
+//     problem
+//   }
+//  }
 
 
-}
+// }
